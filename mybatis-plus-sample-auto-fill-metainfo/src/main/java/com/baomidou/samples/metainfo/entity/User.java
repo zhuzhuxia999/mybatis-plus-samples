c495facc26a68cabd75587cf550cd47817fbe6aa
王一杰 <yijie.wang@apicloud.com>
2019-09-25 10:25:45 +0800

    安装lombok插件后去掉getter/setter

--- mybatis-plus-sample-auto-fill-metainfo/src/main/java/com/baomidou/samples/metainfo/entity/User.java
+++ mybatis-plus-sample-auto-fill-metainfo/src/main/java/com/baomidou/samples/metainfo/entity/User.java
@@ -24,48 +24,6 @@ public class User {
 
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOperator() {
-        return operator;
-    }
-
-    public void setOperator(String operator) {
-        this.operator = operator;
-    }
-
-
-
     public User(Long id, String name, Integer age, String email, String operator) {
         this.id = id;
         this.name = name;
